0c269f1072e03cc6b4639112f122e7b68d8a64f6
Levi S. Drake <levidrakecs@gmail.com>
2022-03-18 13:36:10 -0600

    Exercise2-4 removed

--- Statistician.java
+++ Statistician.java
@@ -28,6 +28,24 @@ public class Statistician {
             largest = input;
         }
     }
+    public static Statistician addStats(Statistician p1, Statistician p2) {
+        Statistician answer = new Statistician();
+        answer.length = p1.length + p2.length;
+        answer.last = p2.last;
+        answer.sum = p1.sum + p2.sum;
+        answer.mean = (p1.mean / 2) + (p2.mean /2);
+        if (p1.smallest < p2.smallest) {
+            answer.smallest = p1.smallest;
+        } else {
+            answer.smallest = p2.smallest;
+        }
+        if (p1.largest > p2.largest) {
+            answer.largest = p1.largest;
+        } else {
+            answer.largest = p2.largest;
+        }
+        return answer;
+    }
 
     public double getLength() {
         return length;
