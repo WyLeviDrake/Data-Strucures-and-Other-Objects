471c65b86156f6a646f72689fb6b6afb450c6f0b
Levi S. Drake <levidrakecs@gmail.com>
2022-04-04 20:53:57 -0600

    Implement all required methods and removed getSequence from DoubleArraySeq, still needs testing.

--- Chapter3/DoubleArraySeq/DoubleArraySeq.java
+++ Chapter3/DoubleArraySeq/DoubleArraySeq.java
@@ -2,47 +2,84 @@ package Chapter3.DoubleArraySeq;
 
 public class DoubleArraySeq {
 
-    private double[] sequence;
-    private int current;
+    private double[] sequence; // Sequence of doubles.
+    private int current; // Current position in the sequence.
+    private int usedElements; // Number of elements used.
 
     public DoubleArraySeq() {
         sequence = new double[10];
         current = 0;
+        usedElements = 0;
     }
     public DoubleArraySeq(int size) {
-        if (size > 0) {
+        if (size > 0 && size < Integer.MAX_VALUE) {
         sequence = new double[size];
         current = 0;
+        usedElements = 0;
         } else {
-            throw new IllegalArgumentException("Only accepts positive numbers");
+            throw new IllegalArgumentException("Only accepts positive numbers less than 2147483647");
         }
     }
     public int size() {
-        return sequence.length;
+        return usedElements;
     }
     public double getCurrent() {
-        return sequence[current];
+        if (isCurrent()) {
+            return sequence[current];
+        } else {
+            throw new IllegalAccessError("This is not a used element.");
+        }
+
     }
+    public boolean isCurrent() {
+        if (current < (usedElements - 1) && current >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // This code isnt actually required and more of a headache than it is worth. Leaving it here to finish later.
+    // public double getSequence(DoubleArraySeq numbers) {
+    //     for (numbers.start(); numbers.isCurrent(); numbers.advance()) {
+    //         System.out.println(sequence[numbers.current]);
+    //     }
+    // }
 
     public void advance() {
-        current += 1;
+        current++;     
     }
     public void start() {
         current = 0;
     }
 
     public void addBefore(double element) {
+        for (int i = usedElements; i > current; i--) {
+            sequence[i] = sequence[i - 1];
+        }
         sequence[current - 1] = element;
+        usedElements++;
     }
-    public void addAfter(double element) {
+    public void addAfter(double element) {   
+        for (int i = usedElements; i > (current + 1); i--) {
+            sequence[i] = sequence[i - 1];
+        }     
         sequence[current + 1] = element;
+        usedElements++;
     }
     public void removeCurrent() {
-
+        if (isCurrent()) {
+            for (int i = current; i < usedElements; i++) {
+                sequence[i] = sequence[i + 1];
+            }
+        }
     }
-
-    public static void main(String[] args) {
-        DoubleArraySeq test = new DoubleArraySeq();
-        System.out.println(test.getCurrent());
+    public void trimToSize() {
+        double[] trimmedSequence;
+        
+        if (sequence.length != usedElements) {
+            trimmedSequence = new double[usedElements];
+            System.arraycopy(sequence, 0, trimmedSequence, 0, usedElements);
+            sequence = trimmedSequence;
+        }
     }
 }
